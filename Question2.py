27fcb73bd5ed55192550cc14c96d57ef565c391e
abhinavkumar1203 <99786254+abhinavkumar1203@users.noreply.github.com>
2025-01-08 19:12:31 +0530

    Question2.py

--- Question2.py
+++ Question2.py
@@ -3,19 +3,24 @@
 
 #Explaination by a simple code snippet : 
 
-import threading
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.contrib.auth.models import User
+import threading #Module to interact with and retrieve information about threads
+from django.db.models.signals import post_save #Signal triggered after a model instance is saved
+from django.dispatch import receiver #connect signal handlers to signals
+from django.contrib.auth.models import #User Import built-in User model
+
 
 @receiver(post_save, sender=User)
 def user_post_save_handler(sender, instance, **kwargs):
+    #As the funcion is connected to the post_save signal of the User model it is automatically executed after a User instance is saved.
     print(f"Signal handler thread: {threading.current_thread().name}")
+    # Prints the thread name where this function is executed
+    # Explaining --> By default, the signal handler runs in the same thread as the caller.
 
 if __name__ == "__main__":
     print(f"Caller thread: {threading.current_thread().name}")
 
     user = User(username="test_user")
-    user.save() 
-
+    
+    # Execute the signal handler (user_post_save_handler) in the same thread
+     user.save() 
 
